d8a6d7f7fa6cd39d1cbf64849b90b6257d49c823
gammmmmble <bob2006tw@gmail.com>
2018-02-09 05:02:30 -0600

    hello:re redundant files

--- hello_world/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/poyiyc2/OSD/hello_world/hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/poyiyc2/OSD/hello_world/hello.ko /home/poyiyc2/OSD/hello_world/hello.o /home/poyiyc2/OSD/hello_world/hello.mod.o
--- hello_world/.tmp_versions/hello.mod
+++
@@ -1,2 +0,0 @@
-/home/poyiyc2/OSD/hello_world/hello.ko
-/home/poyiyc2/OSD/hello_world/hello.o
