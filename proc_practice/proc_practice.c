b9d896d98f5736e5ebe0d0a5eed0a77401c9a98e
gammmmmble <bob2006tw@gmail.com>
2018-02-15 01:43:23 -0600

    proc_practice worked

--- proc_practice/proc_practice.c
+++ proc_practice/proc_practice.c
@@ -21,39 +21,38 @@ static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_entry;
 static char mesg[MAXSIZE];
 
-static ssize_t mp1_read(struct file *file, char __user *buffer, size_t count, loff_t *data){
+static ssize_t mp1_read(struct file *file, char __user *buffer, size_t count, loff_t *offset){
 	//implement
-	int result;
-	result = copy_to_user(buffer, "hello", strlen("hello"));
-	if (result != 0){            // if true then have failed
-		printk(KERN_ALERT "Failed to send %d characters to the user\n", result);
-		return -EFAULT;
+	int result = 0;
+	
+	char *hello = "hello\n";
+	hello += *offset;
+	while(count > 0 && *hello != 0){
+		put_user(*(hello++), buffer++);
+		count--;
+		result++;
 	}
-	printk(KERN_ALERT "Sent %d characters to the user\n", (int)strlen("hello"));
-	return 0;
+	printk(KERN_ALERT "Sent %d characters to the user\n", result);
+	*offset += result;
+	return result;
 }
 
-static ssize_t mp1_write(struct file *file, const char __user *buffer, size_t count, loff_t *data){
+static ssize_t mp1_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset){
 	//implement
-	size_t size = (count > MAXSIZE) ? MAXSIZE : count;
-	int result;
-	result = copy_from_user(mesg, buffer, size);
-	if (result != 0){            // if true then have failed
-		printk(KERN_ALERT "Failed to get %d characters from the user\n", result);
-		return -EFAULT;
+	size_t size;
+	size = MAXSIZE;
+	if(size > count - *offset){
+		size = count - *offset;
 	}
-	printk(KERN_ALERT "Get %ld characters from the user\n", size);
-	/******************samaple****************** /
-	int copied;
-	char*buf;
-	buf = (char*) kmalloc(count, GFP_KERNEL);
-	copied = 0;
-	//put something into the buff, updated copied
-	copy_to_user(buffer, buf, copied);
-	kfree(buf);
-	return copied;
-	/ ******************************************/
-	return 0;
+	if(size > 0){
+		if(copy_from_user(mesg, buffer + *offset, size) != 0){
+			printk(KERN_ALERT "Failed to get %ld characters from the user\n", size);
+			return -EFAULT;
+		}
+		printk(KERN_ALERT "Get %s, %ld characters from the user\n", mesg, size);
+		*offset += size;
+	}
+	return size;
 }
 static const struct file_operations mp1_file = {
 	.owner = THIS_MODULE,
@@ -73,12 +72,14 @@ int __init mp1_init(void){
 		printk(KERN_ALERT "proc_entry create fail\n");
 		return -ENOMEM;
 	}
+	printk(KERN_ALERT "proc_practice installed\n");
 	return 0;
 }
 void __exit mp1_exit(void)
 {
 	remove_proc_entry(FILENAME, proc_dir);
 	remove_proc_entry(DIRECTORY, NULL);
+	printk(KERN_ALERT "proc_practice uninstalled\n");
 }
 module_init(mp1_init);
 module_exit(mp1_exit);
---
+++ proc_practice/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+make
+#sudo rmmod proc_practice
+#sudo insmod proc_practice.ko
+#cat /proc/mp1/status
+#dmesg | tail -5
+#echo 123 > /proc/mp1/status
+#dmesg | tail -5
