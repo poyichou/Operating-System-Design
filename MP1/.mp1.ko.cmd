1c40d3f0f7e78b857655cb81443732e9e5a16937
gammmmmble <bob2006tw@gmail.com>
2018-02-16 18:37:27 -0600

    modify Makefile

--- MP1/.mp1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/poyiyc2/OSD/MP1/mp1.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/poyiyc2/OSD/MP1/mp1.ko /home/poyiyc2/OSD/MP1/mp1.o /home/poyiyc2/OSD/MP1/mp1.mod.o
