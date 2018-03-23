c9278fae8166f05e2df713068fc525058419abd1
gammmmmble <bob2006tw@gmail.com>
2018-03-23 16:40:18 -0500

    MP2:userapp.c ver 1.0

--- MP2/userapp.c
+++ MP2/userapp.c
@@ -6,26 +6,84 @@
 #include <sys/wait.h>
 #include "userapp.h"
 
-int main(int argc, char* argv[])
-{
-	FILE *fp = fopen("/proc/mp1/status", "w");
+int ifexist(int mypid){
+	
+	FILE *fp = fopen("/proc/mp2/status", "r");
 	if(fp == NULL){
-		perror("fopen");
-		return 1;
+		perror("fopen r");
+		exit(1);
 	}
-	pid_t pid = getpid();
-	fprintf(fp, "%d", (int)pid);
-	fclose(fp);
-	//fprintf(stdout, "%d", (int)pid);
-	struct timeval  tv1, tv2;
-	gettimeofday(&tv1, NULL);
-	while(1){
-		gettimeofday(&tv2, NULL);
-		//time out
-		if((double)(tv2.tv_sec - tv1.tv_sec) >= 12.0){
-			break;
+	int pid, state;
+	unsigned long period, computation;
+	while(fscanf(fp, "%d %lu %lu %d", &pid, &computation, &period, &state) != EOF){
+		if(mypid == pid){
+			fclose(fp);
+			return 0;
 		}
 	}
-	execlp("cat", "cat", "/proc/mp1/status", (char*)NULL);
+	fclose(fp);
+	return -1;
+}
+void call_yield(int pid){
+	FILE *fp = fopen("/proc/mp2/status", "w");
+	if(fp == NULL){
+		perror("fopen w");
+		exit(1);
+	}
+	fprintf(fp, "Y %d", (int)pid);
+	fclose(fp);
+}
+void deregister(int pid){
+	FILE *fp = fopen("/proc/mp2/status", "w");
+	if(fp == NULL){
+		perror("fopen w");
+		exit(1);
+	}
+	fprintf(fp, "D %d", (int)pid);
+	fclose(fp);
+}
+int main(int argc, char* argv[])
+{
+	unsigned long period = 60;
+	unsigned long computation = 100;
+	if(argc == 3){
+		period = atoi(argv[1]);
+		computation = atoi(argv[2]);
+	}else if(argc != 1){
+		printf("usage: [programe] [period] [computation] or [program]");
+		return 0;
+	}
+	FILE *fp = fopen("/proc/mp2/status", "w");
+	if(fp == NULL){
+		perror("fopen w");
+		return 1;
+	}
+
+	pid_t pid = getpid();
+	//register
+	fprintf(fp, "R %d %lu %lu", (int)pid, period, computation);
+	fclose(fp);
+	//fprintf(stdout, "%d", (int)pid);
+	if(ifexist((int)pid) == -1){
+		printf("registration failed");
+		return 0;
+	}
+	struct timeval  tv1, tv2;
+	gettimeofday(&tv1, NULL);
+	double last_time = tv1.tv_sec, spent;
+	call_yield((int)pid);
+	while(1){
+		//job
+		gettimeofday(&tv2, NULL);
+		spent = last_time - tv2.tv_sec;
+		printf("wake up time: %lf, timespent: %lf", (double)tv2.tv_sec, spent);
+		last_time = tv2.tv_sec;
+		//time out
+		if((double)(tv2.tv_sec - tv1.tv_sec) >= (computation * 1000)){
+			break;
+		}
+		call_yield((int)pid);
+	}
+	deregister((int)pid);
 	return 0;
 }
