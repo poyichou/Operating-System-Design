be1d4bb60316a78735ff87c4908102b167e7bdf2
gammmmmble <bob2006tw@gmail.com>
2018-04-16 03:45:33 -0500

    MP3 add script for case study 1

---
+++ MP3/case1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#usage: [program] [num]
+num=$1  
+if [ $1 -eq 1 ]; then
+	nice ./work 1024 R 50000 & nice ./work 1024 R 10000
+	./monitor > profile1.data
+elif [ $1 -eq 2 ]; then
+	nice ./work 1024 R 50000 & nice ./work 1024 L 10000
+	./monitor > profile2.data
+fi
